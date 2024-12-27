ac1ba0e7fae86a08d87c3db19431061b5084e3e9
warly <warlyware@pm.me>
2024-12-27 15:40:47 -0800

    Fix useref

--- src/hooks/use-previous.tsx
+++ src/hooks/use-previous.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
 export function usePrevious(value: any) {
-  const ref = useRef();
+  const ref = useRef<any>(null);
 
   useEffect(() => {
     ref.current = value;
