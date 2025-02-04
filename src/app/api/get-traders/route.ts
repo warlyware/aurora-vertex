b0d43bc61fe9cc01433de0d127cf16443c199105
Brett Bowles <brett.bowles@gmail.com>
2025-02-03 19:19:44 -0500

    trader page

--- src/app/api/get-traders/route.ts
+++ src/app/api/get-traders/route.ts
@@ -3,18 +3,17 @@ import { GET_TRADERS } from "@/graphql/queries/get-traders"
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // const { traders }: {
-  //   traders: {
-  //     id: string;
-  //     name: string;
-  //     createdAt: string;
-  //     wallet: {
-  //       id: string;
-  //     };
-  //   }[];
-  // } = await client.request({
-  //   document: GET_TRADERS,
-  // });
+  const { traders }: {
+    traders: {
+      id: string;
+      name: string;
+      wallet: {
+        id: string;
+      };
+    }[];
+  } = await client.request({
+    document: GET_TRADERS,
+  });
 
   const data = await client.request({
     document: GET_TRADERS,
--- src/app/traders/page.tsx
+++ src/app/traders/page.tsx
@@ -2,6 +2,7 @@
 import { PageWrapper } from "@/components/UI/page-wrapper";
 import { BASE_URL } from "@/constants";
 import { useCallback, useEffect, useState } from "react";
+import type { Trader } from '@/types/index';
 
 export default function TradersList() {
   const [ traders, setTraders ] = useState([]);
@@ -9,19 +10,43 @@ export default function TradersList() {
   const fetchTraders = useCallback(async () => {
     const response = await fetch(`${BASE_URL}/api/get-traders`);
     const data = await response.json();
-    setTraders(data);
-    console.log({ data });
+    setTraders(data.body.traders);
+    console.log(data.body.traders);
   }, []);
 
   useEffect(() => {
     fetchTraders();
   }, [fetchTraders]);
 
+  if (!traders.length) {
+    return <div>Loading...</div>
+  }
   return (
     <>
     <PageWrapper>
       <div>
-        this is a div
+        <h2 className="my-8 text-xl">
+          Traders We Follow
+        </h2>
+        {
+          traders.map((trader: Trader) => {
+            return (
+              <div 
+                key={trader.id}
+                className="flex space-x-4"
+              >
+                <h1>{trader.name}</h1>
+                <a 
+                  href={`https://gmgn.ai/sol/address/${trader.wallet?.id}`}
+                  target="_blank"
+                  className="hover:text-green-500 hover:underline"
+                >
+                  {trader.wallet?.id}
+                </a>
+              </div>
+            )
+          })
+        }
       </div>
     </PageWrapper>
     </>
